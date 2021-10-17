d13088be46149dc7ade84fc91b458142ee74ce9e
BhoirSujit <92661835+BhoirSujit@users.noreply.github.com>
2021-10-17 11:24:04 +0530

    Create python_sum.py

---
+++ python_sum.py
@@ -0,0 +1,15 @@
+#first line
+print("Let's sum your two numbers")
+ 
+#sum function
+def sum(a,b) :
+      c = a + b
+      print(c)
+  
+#get value from users
+a = int(input("enter first number "))
+b = int(input("enter second number "))
+
+
+#triggering sum function
+sum(a,b)
