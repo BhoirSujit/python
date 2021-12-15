41b1a333003ff35e72897135842831c201bb36eb
BhoirSujit <92661835+BhoirSujit@users.noreply.github.com>
2021-12-15 16:21:06 +0530

    Add files via upload

---
+++ Modify-string.py
@@ -0,0 +1,18 @@
+a = "Sujit"
+
+#printing all lower case
+print(a.lower())
+
+#printing all upper case
+print(a.upper())
+
+#removing all spaces
+b = "    spaces removed     "
+print(b.strip())
+
+#replacing Su to A
+print(a.replace("Su","A"))
+
+#split
+print(a.split("u"))
+
---
+++ slicing.py
@@ -0,0 +1,13 @@
+a = "sujit bhoir"
+
+#printing jit
+print(a[2:5])
+
+#printing sujit
+print(a[:5])
+
+#printing bhoir
+print(a[6:])
+
+#printing reverse name
+print(a[::-1])
